ee725b2bd424b4674df2cc893758beb322dcb7fc
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2021-04-03 16:31:52 -0700

    Class folder

--- Blockchain.js
+++ Blockchain.js
@@ -36,7 +36,7 @@ class Blockchain{
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
         this.pendingTransactions = [];
-        this.miningReward = 1000000;
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
@@ -82,24 +82,3 @@ class Blockchain{
     }
 
 }
-
-let seairaCoin = new Blockchain();
-seairaCoin.createTransaction(new Transaction('address 1', 'address 2', 100));
-seairaCoin.createTransaction(new Transaction('address 2', 'address 1', 50));
-
-
-
-console.log('\n Starting the Miner 49er...');
-seairaCoin.minePendingTransactions('Jacques-address');
-
-console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress('Jacques-address'));
-
-console.log('\n Starting the Miner 49er...again...');
-seairaCoin.minePendingTransactions('Jacques-address');
-
-console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress('Jacques-address'));
-
-console.log('\n Starting the Miner 49er..yet....again...');
-seairaCoin.minePendingTransactions('Jacques-address');
-
-console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress('Jacques-address'));
