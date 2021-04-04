50e1786c9ca9ad0df6648a77e7e20d57bcde1cb1
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2021-04-03 20:13:24 -0700

    Transaction success

--- Blockchain.js
+++ Blockchain.js
@@ -1,10 +1,38 @@
 const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
 
 class Transaction{
     constructor(fromAddress, toAddress, amount){
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash(){
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(signingKey){
+        if(signingKey.getPublic('hex') !== this.fromAddress){
+            throw new Error('Yeah, You cannot sign transactions for other wallets.');
+        }
+
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64');
+        this.signature = sig.toDER('hex');
+    }
+
+    isValid(){
+        if(this.fromAddress === null) return true;
+
+        if(!this.signature || this.signature.length === 0){
+            throw new Error('No signature in this transaction');
+        }
+
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature);
     }
 }
 class Block{
@@ -29,6 +57,15 @@ class Block{
         console.log("block mined: "+ this.hash);
 
     }
+    hasValidTransactions(){
+        for(const tx of this.transactions){
+            if(!tx.isValid()){
+                return false;
+            }
+        }
+
+         return true;
+    }
 }
 
 class Blockchain{
@@ -48,18 +85,27 @@ class Blockchain{
     }
 
     minePendingTransactions(miningRewardAddress){
+        const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
+        this.pendingTransactions.push(rewardTx);
+
         let block = new Block(Date.now(), this.pendingTransactions);
         block.mineBlock(this.difficulty);
 
         console.log('Block successfully mined!');
         this.chain.push(block);
 
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAddress, this.miningReward)
-        ];
+        this.pendingTransactions = [];
     }
 
-    createTransaction(transaction){
+    addTransaction(transaction){
+
+        if(!transaction.fromAddress || !transaction.toAddress){
+            throw new Error('Transactions must include from and to address');
+        }
+
+        if(!transaction.isValid()){
+            throw new Error('Cannot add invalid transaction to chain');
+        }
         this.pendingTransactions.push(transaction);
     }
 
@@ -79,6 +125,23 @@ class Blockchain{
         }
 
         return balance;
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.lenght; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hasValidTransaction()){
+                return false;
+            }
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const {Blockchain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate('79078ff5f18bd28102901cdc20e109330539786e3901bde6e473ee1bcfedadec');
+const myWalletAddress = myKey.getPublic('hex');
+
+
 let seairaCoin = new Blockchain();
-seairaCoin.createTransaction(new Transaction('address 1', 'address 2', 100));
-seairaCoin.createTransaction(new Transaction('address 2', 'address 1', 50));
+
+const tx1 = new Transaction(myWalletAddress, 'Public key gose here', 10);
+tx1.signTransaction(myKey);
+seairaCoin.addTransaction(tx1);
+
+
+/*seairaCoin.createTransaction(new Transaction('address 1', 'address 2', 100));
+seairaCoin.createTransaction(new Transaction('address 2', 'address 1', 50));*/
 
 
 
 console.log('\n Starting the Miner 49er...');
-seairaCoin.minePendingTransactions('Jacques-address');
+seairaCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress('Jacques-address'));
+console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('\n Starting the Miner 49er...again...');
+/*console.log('\n Starting the Miner 49er...again...');
 seairaCoin.minePendingTransactions('Jacques-address');
 
 console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress('Jacques-address'));
@@ -18,4 +31,4 @@ console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress('Jacques-a
 console.log('\n Starting the Miner 49er..yet....again...');
 seairaCoin.minePendingTransactions('Jacques-address');
 
-console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress('Jacques-address'));
+console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress('Jacques-a'));*/
