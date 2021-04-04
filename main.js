a241ea09fbb8899d48f937ab45a1d184133ae20e
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2021-04-03 20:36:13 -0700

    Code base complete

--- main.js
+++ main.js
@@ -13,6 +13,11 @@ tx1.signTransaction(myKey);
 seairaCoin.addTransaction(tx1);
 
 
+/*const tx2 = new Transaction(myWalletAddress, 'Public key gose here', 85);
+tx2.signTransaction(myKey);
+seairaCoin.addTransaction(tx2);*/
+
+
 /*seairaCoin.createTransaction(new Transaction('address 1', 'address 2', 100));
 seairaCoin.createTransaction(new Transaction('address 2', 'address 1', 50));*/
 
@@ -22,6 +27,10 @@ console.log('\n Starting the Miner 49er...');
 seairaCoin.minePendingTransactions(myWalletAddress);
 
 console.log('\nBalance of Jacques is', seairaCoin.getBalanceOfAddress(myWalletAddress));
+
+console.log('Is chain valid?', seairaCoin.isChainValid());
+
+/*seairaCoin.chain[1].transactions[0].amount = 1;*/
 
 /*console.log('\n Starting the Miner 49er...again...');
 seairaCoin.minePendingTransactions('Jacques-address');
